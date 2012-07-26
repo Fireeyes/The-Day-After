412e8e98f70ff43261d2bd37d196bfd08945f535
Taikand <taikand@yahoo.com>
2012-07-26 15:57:02 +0300

    Added Escape key

--- TheDayAfter_XNA_Project/TheDayAfter_XNA_Project/TheDayAfter_XNA_Project/Game1.cs
+++ TheDayAfter_XNA_Project/TheDayAfter_XNA_Project/TheDayAfter_XNA_Project/Game1.cs
@@ -67,7 +67,7 @@ namespace TheDayAfter_XNA_Project
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) )
                 this.Exit();
 
             // TODO: Add your update logic here
