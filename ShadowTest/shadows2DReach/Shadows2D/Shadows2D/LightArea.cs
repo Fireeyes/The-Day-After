734c2300b0165d665229457cf548b8b7fa5d2d6e
George Popa <geo_007.popa@yahoo.com>
2012-07-27 21:37:23 +0300

    Shadow

---
+++ ShadowTest/shadows2DReach/Shadows2D/Shadows2D/LightArea.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Shadows2D
+{
+    class LightArea
+    {
+        private GraphicsDevice graphicsDevice;
+
+        public RenderTarget2D RenderTarget { get; private set; }
+        public Vector2 LightPosition { get; set; }
+        public Vector2 LightAreaSize { get; set; }
+        public int baseSize;
+
+        public LightArea(GraphicsDevice graphicsDevice, ShadowmapSize size)
+        {
+            baseSize = 2 << (int)size;
+            LightAreaSize = new Vector2(baseSize);
+            RenderTarget = new RenderTarget2D(graphicsDevice, baseSize, baseSize);
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        public Vector2 ToRelativePosition(Vector2 worldPosition)
+        {
+            return worldPosition - (LightPosition - LightAreaSize * 0.5f);
+        }
+
+        public void BeginDrawingShadowCasters()
+        {
+            graphicsDevice.SetRenderTarget(RenderTarget);
+            graphicsDevice.Clear(Color.Transparent);
+        }
+
+        public void EndDrawingShadowCasters()
+        {
+            graphicsDevice.SetRenderTarget(null);
+        }
+    }
+}
---
+++ ShadowTest/shadows2DReach/Shadows2D/Shadows2D/Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Shadows2D
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
+
+
+
+
+
