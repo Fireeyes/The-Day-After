4f34e5701aec82b3849bb3694dd5a5b0964eb543
George Popa <geo_007.popa@yahoo.com>
2012-08-26 13:39:00 +0300

    Fucking finally fixed debug
    NOTE: Do not name Debug Debug
    Fault = Barbutza

---
+++ TheDayAfter_XNA_Project/TheDayAfter_XNA_Project/TheDayAfter_XNA_Project/TestClass/DebugFrame.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+namespace TheDayAfter_XNA_Project
+{
+    public static class DebugFrame
+    {
+        static bool toggle = false;
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            if (InputHandler.ks.IsKeyDown(Keys.F1) && InputHandler.oldks.IsKeyUp(Keys.F1))
+                toggle = !toggle;
+            if (toggle)
+            {
+                string debugText = "Mouse State:\n     " + InputHandler.GetMousePos().ToString();
+
+                if (InputHandler.IsMouseLClick())
+                    debugText += "\n     Left Mouse Click";
+                if (InputHandler.IsMouseRClick())
+                    debugText += "\n     Right Mouse Click";
+                if (InputHandler.IsMouseMClick())
+                    debugText += "\n     Middle Mouse Click";
+
+                debugText += "\nMouse Scroll: \n     " + InputHandler.ScrollValue;
+
+                debugText += "\nPlayer State: \n     " + Player.position + "\n     " + (float)((int)(Player.rotation*1000))/1000;
+                if (InputHandler.Ms.X >= Player.position.X && InputHandler.Ms.X < Player.position.X + 32 &&
+                    InputHandler.Ms.Y >= Player.position.Y && InputHandler.Ms.Y < Player.position.Y + 32)
+                {
+                    
+                }
+                spriteBatch.DrawString(Database.Fonts["debug"], debugText, new Vector2(10, 10), Color.White);
+            }
+
+        }
+    }
+}
